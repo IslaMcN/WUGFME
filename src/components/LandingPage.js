4f8707c45eed456181b413d66d29bdd827dad367
Isla <fallenacademy@gmail.com>
2020-06-08 13:17:16 -0700

    Created Styled Components

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import styled from "styled-components";
 
-const Header = styled.h1`
-color: red;
-`
+import { SectionTop, Body, Header, Navigation, Tag, Main, SectionBottom, SecondHeader, Paragraph, GIF, ThirdHeader, CTA } from './Style/styled-components';
+
+
 
 const LandingPage = () => {
     return(
-        <div>
+        <Body>
             <Header>What U Got For Me Eatz</Header>
-            <nav>
-                <a href="/">Logo</a>
-                <a href="">Sign In</a>
-                <a href="">Register</a>
-            </nav>
-            <body>
-                <section>
-                    <h2>About Us</h2>
-                    <p>Here at What U Got For Me Eatz we understand busy schedules, limited time frames and necessity of ease-of-access. Whether you're a streamer or a doctor, teacher or an emergency care professional, we know that, in the words of April Ludgate, "time is money, money is power, power is pizza and pizza is knowledge."</p>
-                    <iframe src="https://giphy.com/embed/p6iPHyrGYLiRq" width="480" height="192" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/time-money-power-p6iPHyrGYLiRq">via GIPHY</a></p>
-                </section>
-                <section>
-                    <h2>What We Do</h2>
-                    <p>When you send us your personalized form, we will review and create: breakfasts, lunches, dinners and snack & late night cravings ideas based on the Tier and Energy Level you choose.</p>
-                    <p>Based on the level of energy you have or want to put into making your meals, we will include shopping lists of the items you'll want from your local grocery store or grocery delivery service. Or, if you choose, we'll send you step-by-step instructions on how how to make your dish from scratch.</p>
-                    <h3>Be sure to fill out the allergies and likes/dislikes sections as best you can for a more personalized meal making experience.</h3>
-                </section>
-                <button onClick={"/Packages"}>Learn More</button>
-            </body>
-        </div>
+            <Navigation>
+                <Tag href="/">Logo</Tag>
+                <Tag href="">Sign In</Tag>
+                <Tag href="">Register</Tag>
+            </Navigation>
+            <Main>
+                <SectionTop>
+                    <SecondHeader>About Us</SecondHeader>
+                    <Paragraph>Here at What U Got For Me Eatz we understand busy schedules, limited time frames and necessity of ease-of-access. Whether you're a streamer or a doctor, teacher or an emergency care professional, we know that, in the words of April Ludgate, "time is money, money is power, power is pizza and pizza is knowledge."</Paragraph>
+                    <GIF src="https://giphy.com/embed/p6iPHyrGYLiRq" width="480" height="192" frameBorder="0" class="giphy-embed" allowFullScreen></GIF>
+                </SectionTop>
+                <SectionBottom>
+                    <SecondHeader>What We Do</SecondHeader>
+                    <Paragraph>When you send us your personalized form, we will review and create: breakfasts, lunches, dinners and snack & late night cravings ideas based on the Tier and Energy Level you choose.</Paragraph>
+                    <Paragraph>Based on the level of energy you have or want to put into making your meals, we will include shopping lists of the items you'll want from your local grocery store or grocery delivery service. Or, if you choose, we'll send you step-by-step instructions on how how to make your dish from scratch.</Paragraph>
+                    <ThirdHeader>Be sure to fill out the allergies and likes/dislikes sections as best you can for a more personalized meal making experience.</ThirdHeader>
+                </SectionBottom>
+                <CTA onClick={"/Packages"}>Learn More</CTA>
+            </Main>
+        </Body>
     )
 }
 
---
+++ src/components/Style/styled-components.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Body = styled.html`
+
+`
+export const Header = styled.h1`
+
+`
+export const Navigation = styled.nav`
+
+`
+export const Tag = styled.a`
+
+`
+export const Main = styled.body`
+
+`
+export const SectionTop = styled.section`
+
+`
+export const SectionBottom = styled.section`
+
+`
+export const SecondHeader = styled.h2`
+
+`
+export const Paragraph = styled.p`
+
+`
+export const GIF = styled.iframe`
+
+`
+export const ThirdHeader = styled.h3`
+
+`
+export const CTA = styled.button`
+
+`
