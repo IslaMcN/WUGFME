53e8bff79a5c4efa3d90cceead53f04432d63308
Isla <fallenacademy@gmail.com>
2020-06-15 13:33:31 -0700

    Set up forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage.js";
+import { BreakfastForm, LunchForm, DinnerForm } from './components/forms.js';
 
 
 function App() {
   return(
   <div>
       <Route exact path="/" component={LandingPage}/>
+      <Route path="/Breakfast" component={BreakfastForm}/>
+      <Route path="/Lunch" component={LunchForm}/>
+      <Route path="/Dinner" component={DinnerForm}/>
   </div>
     )
 }
--- src/components/forms.js
+++ src/components/forms.js
@@ -1,1 +1,43 @@
-// Redux Form
+import React, {useState} from 'react';
+
+export const BreakfastForm = () => {
+    return(
+        <div>
+            <form>
+                <h2>Breakfast</h2>
+                <label>Likes</label><br></br>
+                <textarea type="text"></textarea><br></br>
+                <label>Dislikes</label><br></br>
+                <textarea ></textarea>
+            </form>
+        </div>
+    )
+}
+
+export const LunchForm = () => {
+    return(
+        <div>
+            <form>
+                <h2>Lunch</h2>
+                <label>Likes</label><br></br>
+                <textarea type="text"></textarea><br></br>
+                <label>Dislikes</label><br></br>
+                <textarea ></textarea>
+            </form>
+        </div>
+    )
+}
+
+export const DinnerForm = () => {
+    return(
+        <div>
+            <form>
+                <h2>Dinner</h2>
+                <label>Likes</label><br></br>
+                <textarea type="text"></textarea><br></br>
+                <label>Dislikes</label><br></br>
+                <textarea ></textarea>
+            </form>
+        </div>
+    )
+}
